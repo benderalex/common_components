d97e11c5a1c23aa673f19b84c43be50b6b2bb6f0
Aleksandr Sidelnik <benderalex5@gmail.com>
2021-02-15 17:10:12 +0200

    fix client

--- src/CashBerry/UnifiedBusClient/Client.php
+++ src/CashBerry/UnifiedBusClient/Client.php
@@ -3,16 +3,64 @@
 namespace CashBerry\UnifiedBusClient;
 
 use CashBerry\UnifiedBusClient\Request\RequestPayload;
-use CashBerry\UnifiedBusClient\Response\HighRiskDbCheckResponse;
+use CashBerry\UnifiedBusClient\Response\ResponsePayload;
 use GuzzleHttp\Exception\RequestException;
 use Karriere\JsonDecoder\JsonDecoder;
+use Psr\Log\LoggerInterface;
 
-
+/**
+ * Class Client
+ * @package CashBerry\UnifiedBusClient
+ */
 class Client
 {
-    private $busEndpoint = 'http://cash_bus:9501';
+    private const LOGGER_GROUP = 'unified_bus_client';
+    /**
+     * @var string
+     */
+    private $busEndpoint;
 
-    public function sendRequest(string $uuid, int $createdAt, string $serviceName, RequestPayload $request)
+    private $factory;
+
+    private $logger;
+
+    /**
+     * Client constructor.
+     * @param string $busEndpoint
+     * @param LoggerInterface $logger
+     * @param $factory
+     */
+    public function __construct(string $busEndpoint, LoggerInterface $logger, $factory)
+    {
+        $this->busEndpoint = $busEndpoint;
+        $this->logger = $logger;
+        $this->factory = $factory;
+    }
+
+    /**
+     * @param $serviceName
+     * @return ResponsePayload
+     * @throws \Exception
+     */
+    private function getResponseByServiceName(string $serviceName): ResponsePayload
+    {
+        $methodName = 'response' . str_replace('-', '', ucwords($serviceName, '-'));
+
+        if (!method_exists($this->factory,  $methodName)) {
+            throw new \Exception("method " . $methodName . "doesn't exist");
+        }
+
+        return $this->factory->$methodName();
+    }
+
+    /**
+     * @param string $uuid
+     * @param int $createdAt
+     * @param string $serviceName
+     * @param RequestPayload $request
+     * @return Response
+     */
+    public function sendRequest(string $uuid, int $createdAt, string $serviceName, RequestPayload $request): Response
     {
         $response = new Response();
         try {
@@ -29,34 +77,61 @@ class Client
                 ]
             );
 
-            $stringResponse = (string)$responseFromBus->getBody();
-            $response = $this->deserialize($stringResponse);
+            $response = $this->deserialize((string)$responseFromBus->getBody(), $serviceName);
             $response->uuid = $uuid;
 
-            return $response;
-
+            $this->logger->info(self::LOGGER_GROUP, [
+                'request' => [
+                    'uuid' => $uuid,
+                    'createdAt' => $createdAt,
+                    'request' => $request->getBody()
+                ],
+                'response' => $response
+            ]);
         } catch (RequestException $exception) {
             if ($exception->hasResponse()) {
-                $response = $this->deserialize((string)$exception->getResponse()->getBody());
+                $response = $this->deserialize((string)$exception->getResponse()->getBody(), $serviceName);
                 $response->uuid = $uuid;
 
-                return $response;
+                $this->logger->error(self::LOGGER_GROUP, [
+                    'request' => [
+                        'uuid' => $uuid,
+                        'createdAt' => $createdAt,
+                        'request' => $request->getBody()
+                    ],
+                    'response' => $response,
+                    'code' => $exception->getCode()
+                ]);
             }
         } catch (\Exception $exception) {
             $response->error = $exception->getMessage();
             $response->code = $exception->getCode();
+            $this->logger->error(self::LOGGER_GROUP, [
+                'request' => [
+                    'uuid' => $uuid,
+                    'createdAt' => $createdAt,
+                    'request' => $request->getBody()
+                ],
+                'code' => $exception->getCode()
+            ]);
 
             return $response;
         }
     }
 
-
-    private function deserialize(string $jsonString)
+    /**
+     * @param string $jsonString
+     * @param string $serviceName
+     * @return mixed
+     * @throws \Exception
+     */
+    private function deserialize(string $jsonString, string $serviceName)
     {
         $jsonDecoder = new JsonDecoder();
         $response = $jsonDecoder->decode($jsonString, Response::class);
         unset($response->details);
-        $response->payload = $jsonDecoder->decode($jsonString, HighRiskDbCheckResponse::class, 'details');
+        $responseObject = $this->getResponseByServiceName($serviceName);
+        $response->payload = $jsonDecoder->decode($jsonString, get_class($responseObject), 'details');
 
         return $response;
     }
--- src/CashBerry/UnifiedBusClient/Response.php
+++ src/CashBerry/UnifiedBusClient/Response.php
@@ -2,18 +2,40 @@
 
 namespace CashBerry\UnifiedBusClient;
 
+/**
+ * Class Response
+ * @package CashBerry\UnifiedBusClient
+ */
 final class Response
 {
+    /**
+     * @var string
+     */
     public $uuid;
-    
+
+    /**
+     * @var int
+     */
     private $createdAt;
 
+    /**
+     * @var string
+     */
     public $payload;
 
+    /**
+     * @var string
+     */
     public $status;
 
+    /**
+     * @var string
+     */
     public $error;
 
+    /**
+     * Response constructor.
+     */
     public function __construct()
     {
         $this->createdAt = time();
