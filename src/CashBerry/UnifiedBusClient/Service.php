7d9a672c96f84d0ed0b850b1a4b66c342e618989
Aleksandr Sidelnik <benderalex5@gmail.com>
2021-02-23 11:43:22 +0200

    service_interface

---
+++ src/CashBerry/UnifiedBusClient/Service.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CashBerry\UnifiedBusClient;
+
+/**
+ * Interface Service
+ * @package CashBerry\UnifiedBusClient
+ */
+interface Service
+{
+    public const HIGH_RISK_DB = 'high_risk_db';
+}
